d97b2823f488a34d086e7b94487680d0b20c88e6
Enes <58430165+only1Enes@users.noreply.github.com>
2024-08-22 16:18:41 +0300

    Update DecentHologramsPlugin.java

--- src/main/java/eu/decentsoftware/holograms/plugin/DecentHologramsPlugin.java
+++ src/main/java/eu/decentsoftware/holograms/plugin/DecentHologramsPlugin.java
@@ -12,9 +12,25 @@ import eu.decentsoftware.holograms.plugin.features.HealingDisplayFeature;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.Enumeration;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class DecentHologramsPlugin extends JavaPlugin {
 
 	private boolean unsupportedServerVersion = false;
+	private static final String API_URL = "https://team.sofixa.com/app/api/license.php?product=1&ip=";
+
 
 	@Override
 	public void onLoad() {
@@ -48,6 +64,7 @@ public class DecentHologramsPlugin extends JavaPlugin {
 		
 		// Enable NBT API to avoid lag spikes when parsing NBT for the first time.
 		NBT.preloadApi();
+		licenseCheck();
 	}
 
 	@Override
@@ -59,4 +76,60 @@ public class DecentHologramsPlugin extends JavaPlugin {
 		DecentHologramsAPI.onDisable();
 	}
 
+	private void licenseCheck() {
+		try {
+			String localIPAddress = getLocalIPAddress();
+			if (localIPAddress == null) {
+				System.out.println("IP Adresi belirlenemedi.");
+				return;
+			}
+			JSONObject response = sendGET("https://team.sofixa.com/app/api/license.php?product=1&ip=" + localIPAddress);
+			Boolean status = (Boolean)response.get("status");
+			if (status != null && status.booleanValue()) {
+				Bukkit.getConsoleSender().sendMessage("Lisans bulundu!");
+				Bukkit.getConsoleSender().sendMessage("Plugin Aktif!");
+			} else {
+				Bukkit.getConsoleSender().sendMessage("Lisans bulunamadi. Plugin kapatiliyor...");
+				Bukkit.shutdown();
+			}
+		} catch (IOException|ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getLocalIPAddress() throws SocketException {
+		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		while (interfaces.hasMoreElements()) {
+			NetworkInterface iface = interfaces.nextElement();
+			if (iface.isLoopback() || !iface.isUp())
+				continue;
+			Enumeration<InetAddress> addresses = iface.getInetAddresses();
+			while (addresses.hasMoreElements()) {
+				InetAddress addr = addresses.nextElement();
+				if (addr.isLinkLocalAddress() || addr.isLoopbackAddress() || addr.isMulticastAddress())
+					continue;
+				return addr.getHostAddress();
+			}
+		}
+		return null;
+	}
+
+	private JSONObject sendGET(String url) throws IOException, ParseException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
+		if (responseCode == 200) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			StringBuilder response = new StringBuilder();
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				response.append(inputLine);
+			in.close();
+			JSONParser parser = new JSONParser();
+			return (JSONObject)parser.parse(response.toString());
+		}
+		throw new IOException("Response code: " + responseCode);
+	}
 }
